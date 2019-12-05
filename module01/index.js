806703dad4579085e482023add171edaf5d23058
Victor Loyola <victor.tostes@gmail.com>
2019-12-05 19:03:35 -0300

    Added nodemon and some http requests

--- module01/index.js
+++ module01/index.js
@@ -2,18 +2,43 @@ const express = require('express');
 
 const server = express();
 
-//server.use()
+server.use(express.json());
 
 //Query params = ?teste=1 => req.query
 //Route params = /users/1 => req.params
 //Requet body = { "name": "Victor". "email": "victor.tostes@gmail.com" }
 
-const users = ['Andrés','Maria','Marina'];
+const users = ['Andrés','Victor','Marina'];
 
+//List all users
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+//Create user
+server.post("/users", (req,res) => {
+  const { name } = req.body;
+
+  users.push(name);
+  
+  return res.json(users);
+});
+
+//List user by id
 server.get("/users/:index", (req, res) => {
   const { index } = req.params;
   
   return res.json(users[index]);
+});
+
+//Update user
+server.put("/users/:index", (req,res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
 });
 
 server.listen(3000);
